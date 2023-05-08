e945f21460a7814d996d9d6378ebe657e9798420
yoon <hayashitaro1902@gmail.com>
2023-05-08 12:40:52 +0000

    create tables

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,15 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('rate');
+            $table->string('position');
+            $table->string('pace');
+            $table->string('shoot');
+            $table->string('pass');
+            $table->string('drible');
+            $table->string('defence');
+            $table->string('physical');
+            $table->text('content');
             $table->rememberToken();
             $table->timestamps();
         });
